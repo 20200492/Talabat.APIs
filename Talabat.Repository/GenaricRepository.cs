417fb016732a698d3c73d2f60c9f712fe5bddbe6
20200492 <mahmoudhashem20112002@gmail.com>
2024-04-11 23:55:31 +0200

    Generic Repository

---
+++ Talabat.Repository/GenaricRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contruct;
+
+namespace Talabat.Repository
+{
+    public class GenaricRepository<T> : IGenaricRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbContext;
+
+        public GenaricRepository(StoreContext dbContext)
+        {
+            _dbContext = dbContext;  
+        }
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public Task<T> GetAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+    }
+}
