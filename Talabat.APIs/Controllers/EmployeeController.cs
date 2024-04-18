451d22d98fbd73dfe51c673b35b9cab9fba90e13
20200492 <mahmoudhashem20112002@gmail.com>
2024-04-18 17:55:42 +0200

    Employee Example - Draft

---
+++ Talabat.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contruct;
+using Talabat.Core.Specification.Employees.Spec;
+
+namespace Talabat.APIs.Controllers
+{
+    public class EmployeeController : BaseAPIController
+    {
+        private readonly IGenaricRepository<Employee> _GenaricRepo;
+
+        public EmployeeController(IGenaricRepository<Employee> GenaricRepo)
+        {
+            _GenaricRepo = GenaricRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var Spec = new EmployeesWithDepartmentSpecifications();
+            var employees = _GenaricRepo.GetAllWithSpecAsync(Spec);
+
+            return Ok(employees);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeeById(int id)
+        {
+            var Spec = new EmployeesWithDepartmentSpecifications(id);
+            var employee = _GenaricRepo.GetWithSpecAsync(id,Spec);
+
+            if (employee is null)
+                return NotFound(new { StatusCode = 404, Message = "Not Found" }); // 404
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ Talabat.Core/Specification/Employees.Spec/EmployeesWithDepartmentSpecifications.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Specification.Employees.Spec
+{
+    public class EmployeesWithDepartmentSpecifications : BaseSpecifications<Employee>
+    {
+        public EmployeesWithDepartmentSpecifications() :base()
+        {
+            Includes.Add(E =>  E.Department);
+        }
+
+        public EmployeesWithDepartmentSpecifications(int id) : base (E => E.Id == id)
+        {
+            Includes.Add(E => E.Department);
+        }
+    }
+}
